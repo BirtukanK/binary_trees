b7dc8dfa930130d61e115e130daf83af6bde49b4
SundayMba <sundinoh@gmail.com>
2024-01-03 18:03:45 +0100

    task 0

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -9,7 +9,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *newnode;
 
-	newnode = (binary_tree_t*)malloc(sizeof(binary_tree_t));
+	newnode = (binary_tree_t *)malloc(sizeof(binary_tree_t));
 	newnode->left = NULL;
 	newnode->right = NULL;
 	newnode->parent = parent;
--- binary_trees.h
+++ binary_trees.h
@@ -1,6 +1,8 @@
 #ifndef HEADER
 #define HEADER
 
+#include <stdlib.h>
+
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -19,6 +21,8 @@ struct binary_tree_s
 
 typedef struct binary_tree_s binary_tree_t;
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
+void binary_tree_print(const binary_tree_t *tree);
 
 
 #endif
