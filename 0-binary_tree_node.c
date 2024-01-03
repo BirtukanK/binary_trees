53ebb611e4aabad780f9888fa99571bff8e42340
SundayMba <sundinoh@gmail.com>
2024-01-03 18:06:48 +0100

    checked for NULL

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -10,6 +10,8 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	binary_tree_t *newnode;
 
 	newnode = (binary_tree_t *)malloc(sizeof(binary_tree_t));
+	if (newnode == NULL)
+		return (NULL);
 	newnode->left = NULL;
 	newnode->right = NULL;
 	newnode->parent = parent;
